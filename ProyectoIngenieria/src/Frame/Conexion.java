ab46b6885e0561f354075e218da42f81a4858e86
becario <becario@PCBECARIO-PC.w32.com.mx>
2015-11-27 18:21:50 -0600

    27/11/2015

---
+++ ProyectoIngenieria/src/Frame/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Frame;
+
+
+import java.sql.*;
+
+public class Conexion {
+   private static Connection conect = null;
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if ( conect== null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conect = DriverManager.getConnection("jdbc:mysql://localhost/java_mysql", "root", "");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return conect;
+   }
+   public static void cerrar() throws SQLException {
+      if (conect!= null) {
+         conect.close();
+     
+      }
+   
+   }   
+}
